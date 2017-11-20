759e38abafaa47ebea68905645ccf5579a84c745
ewaSaputraMahardika <ewasaputra03@gmail.com>
2017-11-20 12:01:48 +0700

    membuat XML

---
+++ app/src/main/java/com/example/smkm2p/utsmobile/HasilActivity.java
@@ -0,0 +1,13 @@
+package com.example.smkm2p.utsmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HasilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+    }
+}
